fb5287fbb798212ff7d21f57b1c9002c39658dc8
zhangshiyu <1946955309@qq.com>
2022-05-23 00:11:28 +0800

    编写第2个测试用例：第2个测试用例，全部1分，得到所有分数相加，报错

--- src/test/java/com/example/TDDLearnning/BowlingGameTest.java
+++ src/test/java/com/example/TDDLearnning/BowlingGameTest.java
@@ -5,12 +5,28 @@ import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
 
+    /**
+     * 第一个测试用例 全部投0分 返回0分
+     * @throws Exception
+     */
     public void testGutterGame() throws Exception {
         Game g = new Game();
         for (int i = 0; i < 20; i++) {
             g.roll(0);
         }
 
-        assertEquals(0,g.score());
+        assertEquals(0, g.score());
+    }
+
+    /**
+     * 第2个测试用例，全部1分，得到所有分数相加
+     * @throws Exception
+     */
+    public void testAllOnes() throws Exception {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++) {
+            g.roll(1);
+        }
+        assertEquals(20, g.score());
     }
 }
