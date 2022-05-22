c05426fd0511baf22a1ca895aea97993a7e74fe5
zhangshiyu <1946955309@qq.com>
2022-05-23 00:17:44 +0800

    方法过丑美化一下，将重复代码封装

--- src/test/java/com/example/TDDLearnning/BowlingGameTest.java
+++ src/test/java/com/example/TDDLearnning/BowlingGameTest.java
@@ -5,20 +5,33 @@ import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
 
+    private Game g;
+
+    /**
+     * 跑每个测试单元前，都会执行的方法
+     * @throws Exception
+     */
+    @Override
+    protected void setUp() throws Exception {
+        g = new Game();
+    }
+
     /**
      * 第一个测试用例 全部投0分 返回0分
      *
      * @throws Exception
      */
     public void testGutterGame() throws Exception {
-        Game g = new Game();
         int n = 20;
         int pins = 0;
+        rollMany(n, pins);
+        assertEquals(0, g.score());
+    }
+
+    private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++) {
             g.roll(pins);
         }
-
-        assertEquals(0, g.score());
     }
 
     /**
@@ -27,7 +40,7 @@ public class BowlingGameTest extends TestCase {
      * @throws Exception
      */
     public void testAllOnes() throws Exception {
-        Game g = new Game();
+
         for (int i = 0; i < 20; i++) {
             g.roll(1);
         }
