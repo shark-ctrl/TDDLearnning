0e36765ac4c90c857d7c89ee7c6092e6f1433aa8
zhangshiyu <1946955309@qq.com>
2022-05-23 00:12:29 +0800

    编写第2个测试用例：第2个测试用例，全部1分，得到所有分数相加，成功解决

--- src/main/java/com/example/TDDLearnning/BowlingGame/Game.java
+++ src/main/java/com/example/TDDLearnning/BowlingGame/Game.java
@@ -1,11 +1,13 @@
 package com.example.TDDLearnning.BowlingGame;
 
 public class Game {
+    private int score=0;
 
     public void roll(int pins) {
+        score+=pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
