726de28ee551fc5f3e708785b05b19bb133f1a12
zhangshiyu <1946955309@qq.com>
2022-05-23 00:42:09 +0800

    testOneSpare测试用例修复失败,重构改善既有效的代码

--- src/main/java/com/example/TDDLearnning/BowlingGame/Game.java
+++ src/main/java/com/example/TDDLearnning/BowlingGame/Game.java
@@ -12,16 +12,14 @@ public class Game {
 
     public int score() {
         int score = 0;
-        for (int i = 0; i < rolls.length; i++) {
-            /**
-             * 错误代码，假如第一局分数为1 5，第2局为5 ,5 这段代码会使得第一局第2球分数+第2局第一球分数相加触发spare
-             * if (rolls[i] + rolls[i+1] == 10) // spare
-             *                 score += ...
-             */
-
-
-            score += rolls[i];
+        int i = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            //成绩等于每局的两球成绩相加
+            score += rolls[i] + rolls[i + 1];
+            //越过这局的分数
+            i += 2;
         }
+
         return score;
     }
 }
