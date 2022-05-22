34a381b5ab594f018cd4564e218281f7e8e2cde6
zhangshiyu <1946955309@qq.com>
2022-05-22 23:57:46 +0800

    项目初始化

---
+++ src/main/java/com/example/TDDLearnning/TddLearnningApplication.java
@@ -0,0 +1,13 @@
+package com.example.TDDLearnning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddLearnningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddLearnningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TDDLearnning/TddLearnningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TDDLearnning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddLearnningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
